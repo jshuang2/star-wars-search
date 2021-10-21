d8c8f7372022654e921378a6cea98f17d95b2bb8
Jeff Huang <jeffshuang2@gmail.com>
2021-10-21 00:37:03 -0700

    Added headers and metadata to API calls and updated README.

--- README.md
+++ README.md
@@ -1,30 +1,36 @@
-# Front-End Developer Test
+# Star Wars Character API
 
 ## Overview
-Your client has tasked you with creating an application to view the people of the Star Wars
-universe, with the following requirements:
+This application utilizes a free Star Wars API and allows the user to search for a character from the films and view data on them.
 
-* List people, with the ability to search by name
-* View the details for a given person, including their film appearances and species
 
-### Keep in mind the following questions
+### Technologies Used
+- React
+- TypeScript
+- Bootstrap
 
-* What design patterns did you use?
-* How would you test your application?
-* How do you manage/store application state?
-* What ways could you structure the code to make it easy to understand and maintain?
-* What other considerations and tradeoffs did you make when building the application?
-* How are you describing and typing API data?
+---
 
-## Getting Started
-The starter app includes a minimal api layer and people list view. The API documentation can be found at https://swapi.dev and requires no extra setup.
+### Installation and Setup Instructions
 
-### Running the app
-The app was created via `create-react-app` and has everything you'll need to build and test.
 
-Run the app with `yarn start` or `npm run start`.
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
 
-Test the app with `yarn test` or `npm run test`.
+Installation:
 
-## Submitting Your App
-When you have completed your app, please create public repository and send us a link - GitHub, GitLab, BitBucket etc.
+`npm install`  
+
+To Start Server:
+
+`npm run start` or `yarn start`  
+
+To Test App: 
+
+`npm run test` or `yarn test`
+
+To Visit App:
+
+`localhost:3000/`  
+
+---
+
--- src/api/index.ts
+++ src/api/index.ts
@@ -18,7 +18,7 @@ export async function fetchJson<Response = any>(url: string, init?: RequestInit)
 
 
 // API GET call that takes in a character's name and returns data on that character.
-export async function fetchCharacter(url: string, init?: RequestInit){
+export async function fetchCharacter<Response = any>(url: string, init?: RequestInit): Promise<Response>{
 
   const response = await 
   fetch(`https://swapi.dev/api/people/?search=${url}`, {
@@ -42,16 +42,22 @@ export async function fetchCharacter(url: string, init?: RequestInit){
 
 
 // API GET call that takes in an array of film endpoints and returns the titles of each film in a new array.
-export async function fetchFilms(films: string[]) {
+export async function fetchFilms(films: string[], init?: RequestInit) {
 
   const filmtitlesArray: string[] = []
 
   for (let film of films) {
-    await fetch(`${film}`).then(response => {
-      return response.json()
+    await fetch(`${film}`, {
+      ...init ?? {},
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(response => {
+        return response.json()
     })
-    .then(data => {
-      filmtitlesArray.push(data.title)
+      .then(data => {
+        filmtitlesArray.push(data.title)
     })
   }
 
@@ -62,16 +68,22 @@ export async function fetchFilms(films: string[]) {
 
 
 // API GET call that takes in an array of species endpoints and returns the name of each species in a new array.
-export async function fetchSpecies(speciesArray: string[]) {
+export async function fetchSpecies(speciesArray: string[], init?: RequestInit) {
 
   const speciesResult: string[] = []
 
   for (let specie of speciesArray) {
-    await fetch(`${specie}`).then(response => {
-      return response.json();
+    await fetch(`${specie}`, {
+      ...init ?? {},
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(response => {
+        return response.json();
     })
-    .then(data => {
-      speciesResult.push(data.name)
+      .then(data => {
+        speciesResult.push(data.name)
     })
   }
 
